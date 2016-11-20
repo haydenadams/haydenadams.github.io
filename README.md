8d748845084f3208cea824e065dcdb66cc6ab99f
Karl Floersch <karl@karlfloersch.com>
2016-11-19 19:38:13 -0500

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# What We See
+
+A dope experiment
---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('test');
+console.log('testestiewoj');
+
+// $( document ).ready(function() {
+// });
+
+
+// TODO: Add google analytics
